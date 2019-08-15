2a154ee085759267c0c78514edb923330ee4de98
Dmitriy Mullo <d.a.mullo1981@gmail.com>
2019-08-15 21:24:27 +0300

    Работа с кодом edit DeduplicatorProcessor.java edit DeduplicatorProcessorTest.java
    файл хранения данных кэша
    add deduplicated_etalon.data
    
    файл хранения данных кэша для правильной работы тестов
    add deduplicated_etalon_batch.data

--- src/main/java/com/job/applicants/aptitude/test/DeduplicatorProcessor.java
+++ src/main/java/com/job/applicants/aptitude/test/DeduplicatorProcessor.java
@@ -1,5 +1,17 @@
 package com.job.applicants.aptitude.test;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.processor.AbstractProcessor;
 import org.apache.nifi.processor.ProcessContext;
@@ -10,9 +22,17 @@ import org.apache.nifi.processor.exception.ProcessException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
 
+@Tags({"DEDUPLICATE"})
+@CapabilityDescription("All duplicates dead here")
 public class DeduplicatorProcessor extends AbstractProcessor {
+
     public static final Relationship SUCCESS = new Relationship.Builder()
             .name("SUCCESS")
             .description("Success relationship")
@@ -34,33 +54,136 @@ public class DeduplicatorProcessor extends AbstractProcessor {
     @Override
     public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
         //implement your code here
-        FlowFile flowfile = session.get();
-/*
-        dead duplicates
-        
-        вход: строка с разделителями
-        
-        инвариант:
-        > разделитель \n
-        > сплит не содержит разделитель
-        > все объекты в кэше уникальны
-        
-        контракт:
-        > пустая строка означает отсутствие данных на входе (в сплите)
-        
-        последовательность действий:
-        создать буфер
-        цикл (получить сплиты, получить первый/следующий сплит, продолжать, пока есть следующий):
-            тест
-                сплит пуст ?
-                    да: тест завершен
-                    нет:
-                        insert! в кэш (побочный эффект - модификация кэша)
-                        insert! удался ?
-                            да: записать сплит в буфер
-        буфер не пуст ?
-            да: записать буфер в ff
+        FlowFile flowFile = session.get();
+
+        if (flowFile == null) {
+            return;
+        }
+
+        // простой файловый кэш
+        SimpleFileCache cache = new SimpleFileCache();
+
+        // словарь для хранения строк, переживших когда-либо deduplication
+        final Set<String> dict = cache.load();
+        // список строк из текущего ff, переживших deduplication
+        final List<String> splits = new LinkedList<>();
+
+        session.read(flowFile, (final InputStream rawIn) -> {
+            try (final BufferedReader reader = new BufferedReader(new InputStreamReader(rawIn))) {
+                /**
+                 * построчное чтение содержимого ff
+                 * пустые строки не добавляются в список
+                 * строки, уже находящиеся в кэше, не добавляются в список
+                 */
+                String split = reader.readLine();
+                while (split != null) {
+                    if (!split.isEmpty() && dict.add(split)) {
+                        splits.add(split);
+                    }
+                    split = reader.readLine();
+                }
+            }
+            // обновить кэш
+            cache.save(dict);
+        });
+
+        // строки, пережившие deduplication, находятся в списке?
+        if (splits.size() > 0) {
+            // да: объединить содержимое списка ч/з разделитель, записать результат в текущий ff
+            flowFile = session.write(flowFile, (OutputStream out) -> {
+                out.write(splits.stream().collect(Collectors.joining("\n")).getBytes());
+            });
+            session.transfer(flowFile, SUCCESS);
+        } else {
+            // иначе удалить ff
+            session.remove(flowFile);
+        }
+    }
+}
+
+/**
+ * Интерфейс операций кэша.
+ *
+ */
+interface CacheSetOperation<T> {
+
+    /**
+     * Обновить кэш.
+     *
+     * @param  data
+     *         записываемые данные кэша как объекта, реализующего интерфейс {@code Set<T>}
+     */
+    public void save(Set<T> data);
+
+    /**
+     * Загрузить кэш.
+     * 
+     * @return {@code Set<T>} объект кэша, реализующего интерфейс {@code Set<T>}
+     *
+     */
+    public Set<T> load();
+}
+
+/**
+ * Простой класс реализации операций кэша.
+ *
+ */
+class SimpleFileCache implements CacheSetOperation<String> {
+    
+    // каталог и имя файла для хранения данных кэша
+    static Path path;
+    
+    static {
+        /**
+        * инициализация рабочим каталогом пользователя (в win10 это корневой каталог проекта или экземпляра NiFi)
+        * и именем файла по-умолчанию "deduplicated_etalon.data"
+        *
         */
-        session.transfer(flowfile, SUCCESS);
+        path = Paths.get(System.getProperty("user.dir"), "deduplicated_etalon.data");
+    }
+
+    /**
+    * Обновить кэш.
+    *
+    * @param  data
+    *         записываемые данные кэша как объекта, реализующего интерфейс {@code Set<String>}
+    * 
+    * сохранение данных кэша выполняется строка за строкой
+    */
+    @Override
+    public void save(Set<String> data) {
+        try {
+            // сохранение данных кэша выполняется строка за строкой
+            try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path.toFile())))) {
+                for (String line : data) {
+                    bw.write(line);
+                    bw.newLine();
+                }
+                bw.flush();
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /**
+     * Загрузить кэш.
+     * 
+     * @return {@code Set<String>} объект кэша, реализующего интерфейс {@code Set<String>}
+     *
+     */
+    @Override
+    public Set<String> load() {
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path.toFile())))) {
+            return new HashSet<>(reader.lines().collect(Collectors.toSet()));
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+            return new HashSet<>();
+        } catch (IOException ex) {
+            System.out.println(ex);
+            return new HashSet<>();
+        }
     }
 }
--- src/test/java/com/job/applicants/aptitude/test/DeduplicatorProcessorTest.java
+++ src/test/java/com/job/applicants/aptitude/test/DeduplicatorProcessorTest.java
@@ -1,5 +1,10 @@
 package com.job.applicants.aptitude.test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import org.apache.nifi.util.MockFlowFile;
 import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
@@ -9,44 +14,66 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeduplicatorProcessorTest {
 
-    private TestRunner testRunner = TestRunners.newTestRunner(DeduplicatorProcessor.class);
+    private final TestRunner testRunner = TestRunners.newTestRunner(DeduplicatorProcessor.class);
+
+    void dropCache() {
+        try {
+            Files.deleteIfExists(Path.of(System.getProperty("user.dir"), "deduplicated_etalon.data"));
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+    
+    void replaceCacheByBatch() {
+        try {
+            Path from = Paths.get(System.getProperty("user.dir"), "deduplicated_etalon_batch.data");
+            Path to = Paths.get(System.getProperty("user.dir"), "deduplicated_etalon.data");
+            Files.copy(from, to, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ex) {
+            Logger.getLogger(DeduplicatorProcessorTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     @Test
     @DisplayName("Processor should remove duplicates for new batch")
     void batchDeduplicateTest() {
+        dropCache();
         byte[] testData = "TAG1;1561025065;0.1;3\nTAG1;1561025065;0.1;3\nTAG2;1561025998;0.3;3".getBytes();
         testRunner.enqueue(testData);
         testRunner.run();
         List<MockFlowFile> flowFiles = testRunner.getFlowFilesForRelationship(DeduplicatorProcessor.SUCCESS);
         List<String> result = flowFiles.stream()
                 .map(a -> new String(testRunner.getContentAsByteArray(a)))
-                .map(a -> new LinkedList<String>(Arrays.asList(a.split("\n"))))
+                .map(a -> new LinkedList<>(Arrays.asList(a.split("\n"))))
                 .flatMap(List::stream)
                 .collect(Collectors.toList());
 
-        assertEquals(result.size(), 2);
+        assertEquals(2, result.size());
     }
 
     @Test
     @DisplayName("Processor should remove duplicates for all new batches")
     void newBatchDeduplicateTest() {
+        replaceCacheByBatch();
         byte[] testData = "TAG2;1561025998;0.3;3\nTAG3;1561025998;0.9;2".getBytes();
         testRunner.enqueue(testData);
         testRunner.run();
         List<MockFlowFile> flowFiles = testRunner.getFlowFilesForRelationship(DeduplicatorProcessor.SUCCESS);
         List<String> result = flowFiles.stream()
                 .map(a -> new String(testRunner.getContentAsByteArray(a)))
-                .map(a -> new LinkedList<String>(Arrays.asList(a.split("\n"))))
+                .map(a -> new LinkedList<>(Arrays.asList(a.split("\n"))))
                 .flatMap(List::stream)
                 .collect(Collectors.toList());
 
-        assertEquals(result.size(), 1);
+        assertEquals(1, result.size());
     }
 
 }
